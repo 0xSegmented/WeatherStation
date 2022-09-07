7963fdf4d5c058dcf28ef72b644f9087eb5ab49b
xatt <111096156+MaliciousXatt@users.noreply.github.com>
2022-09-07 10:06:56 +0200

    Add files via upload

---
+++ WeatherStation.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int choice;
+  double degree;
+  double fahrenheit;
+  double celcius;
+  printf("Choose Celcius=1 or Fahrenheit=2:\n");
+  scanf("%d", &choice);
+  if (choice == 1) {
+    printf("Enter Celcius:\n");
+    scanf("%lf", &degree);
+    fahrenheit = (degree * 1.8) + 32;
+    printf("Temprature in Fahrenheit = %lf", fahrenheit);
+  } else if (choice == 2) {
+    printf("Enter Fahrenheit:\n");
+    scanf("%lf", &degree);
+    celcius = (degree - 32) * .5556;
+    printf("Temprature in Celcius = %lf", celcius);
+  }
+  return 0;
+}
